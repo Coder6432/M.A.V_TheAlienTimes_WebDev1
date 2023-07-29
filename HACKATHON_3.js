e6528f9b3955f4ca2fa85324d21689845a695201
Coder6432 <88219330+Coder6432@users.noreply.github.com>
2023-07-29 16:14:02 +0800

    Add files via upload

---
+++ HACKATHON_3.js
@@ -0,0 +1,9 @@
+function defy(){
+    window.location.href='defense.html';
+};
+function weapon2(){
+    window.location.href='WEAPONS.html';
+};
+function resource2(){
+    window.location.href='resources.html';
+};
